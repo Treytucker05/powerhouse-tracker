29633a793ade1fba8481a142a778574b78bc83f5
Treytucker05 <treytucker05@yahoo.com>
2025-06-13 16:53:09 -0500

    Add Jest testing framework and volume algorithm test

---
+++ js/algorithms/__tests__/volume.test.js
@@ -0,0 +1,6 @@
+import { weeklyVolume } from '../volume.js';
+
+test('weeklyVolume sums set counts', () => {
+  const week = [{ sets: 10 }, { sets: 5 }];
+  expect(weeklyVolume(week)).toBe(15);
+});
