35c23e32c14c2d739e23b6c530e1786c356b37a5
Treytucker05 <treytucker05@yahoo.com>
2025-06-13 17:06:43 -0500

    Switch to ES modules: add type:module and update test imports

--- js/algorithms/__tests__/volume.test.js
+++ js/algorithms/__tests__/volume.test.js
@@ -1,4 +1,4 @@
-const { weeklyVolume } = require('../volume.js');
+import { weeklyVolume } from '../volume.js';
 
 test('weeklyVolume sums set counts', () => {
   const week = [{ sets: 10 }, { sets: 5 }];
