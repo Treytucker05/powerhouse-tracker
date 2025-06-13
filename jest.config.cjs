60159a814c7a4f0d279c4c060bb9e72db5d5df74
Treytucker05 <treytucker05@yahoo.com>
2025-06-13 17:02:15 -0500

    Remove jest.config.cjs - use Jest defaults

--- jest.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.js'], // treat plain .js files as ES-modules
-  transform: {}                    // no Babel
-};
