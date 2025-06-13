cea67d896ec9246cf75024e891e083642ecc5da5
Treytucker05 <treytucker05@yahoo.com>
2025-06-13 17:00:48 -0500

    Update volume test to use CommonJS require syntax

--- js/algorithms/__tests__/volume.test.js
+++ js/algorithms/__tests__/volume.test.js
@@ -1,4 +1,4 @@
-import { weeklyVolume } from '../volume.js';
+const { weeklyVolume } = require('../volume.js');
 
 test('weeklyVolume sums set counts', () => {
   const week = [{ sets: 10 }, { sets: 5 }];
