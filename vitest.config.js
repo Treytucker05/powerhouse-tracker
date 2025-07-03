688b84be5f83e2fbb9b858b0ac23f3a8367f8425
Treytucker05 <treytucker05@yahoo.com>
2025-07-03 18:58:13 -0500

    test env setup

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vitest/config'
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { fileURLToPath } from 'url'
+import path from 'path'
+import dotenv from 'dotenv'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: path.join(__dirname, '.env.test') })
+}
 
 export default defineConfig({
   test: {
